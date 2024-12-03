0d9ee184533d0720d0a7c7d4e093ab16870899fb
shenllyz <102724218+shenllyz@users.noreply.github.com>
2024-12-03 00:58:25 -0800

    search icon removed

--- app/src/main/java/com/example/weatherapp/view/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/view/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.weatherapp.view
 
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.widget.SearchView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -15,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
